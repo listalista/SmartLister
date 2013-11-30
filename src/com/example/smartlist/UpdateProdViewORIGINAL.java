056dc480e694f0649855c715683f5b8c0c7e233c
Marc Streeter <marc.streeter@mavs.uta.edu>
2013-11-30 08:59:37 -0600

    Keeping a file just in case

---
+++ src/com/example/smartlist/UpdateProdViewORIGINAL.java
@@ -0,0 +1,41 @@
+package com.example.smartlist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+ 
+public class UpdateProdViewORIGINAL extends Activity implements OnItemClickListener {
+    
+	ListView listView; /*Declaring Listview in public*/
+ 
+	/*Default Method which will run first*/
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_list_view);  /*Retrieving the contents of the layout update_list_view*/
+ 
+        listView = (ListView) findViewById(R.id.updateprodlist); /*Declaring the List View*/
+        
+        /*Retrieving the array content from strings.xml file*/
+        String[] updateprod = getResources().getStringArray(R.array.Temp_Prod_List);
+    
+        /*Pushing the array content into list view and capture the onClick action*/
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, updateprod));
+        listView.setOnItemClickListener(this);
+    }
+ 
+    /*onClickListener action upon clicking an option from the list*/
+    @Override
+    public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+    	
+    	
+    	     Intent intent = new Intent(UpdateProdViewORIGINAL.this, UpdateProdTitle.class);
+    	     startActivity(intent);                  
+    	
+    }
+}
